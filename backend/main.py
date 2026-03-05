983a6a26fcf787c00b11c55d1734f61d020f30fc
Ashmit <ashmitv704@gmail.com>
2026-03-06 02:46:58 +0530

    Completed the Auth module and added the issue reporting endpoint with image storage

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from  routes.auth_routes import router as auth_router
+from routes.user_routes import router as user_router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.include_router(user_router)
+
 app.include_router(auth_router)
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
